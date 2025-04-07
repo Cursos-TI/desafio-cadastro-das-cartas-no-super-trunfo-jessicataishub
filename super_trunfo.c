73b46958722536fa7348178b24ef7b7851331a04
jessicataishub <202501395309@alunos.estacio.br>
2025-04-07 17:33:09 +0000

    cadastrei super trunfo

---
+++ super_trunfo.c
@@ -0,0 +1,46 @@
+# include <stdio.h>
+
+int main() {
+   // Definição das variáveis para a Carta 1
+   //estado1(char)[bahia];
+  //codigodacarta1(char)[A01];
+   //nomecidade1(char)[salvador];
+   //populacao1(int)[20000000];
+   // area1(float)[300000000];
+// pib1(float)[400000000];
+   // numeropontosturisticos1(foat)[51];
+   
+   // Exibição das informações da Carta 1
+   //printf("Carta 1:\n");
+   //printf("Estado: %c\n", estado1);
+   //printf("Código: %s\n", codigo1);
+   //printf("Nome da Cidade: %s\n", cidade1);
+   //printf("População: %d\n", populacao1);
+   //printf("Área: %.2f km²\n", area1);
+   //printf("PIB: %.2f bilhões de reais\n", pib1);
+   //printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
+   
+   printf("\n");
+
+
+   // Definição das variáveis para a Carta 2
+   //estado2(char)[ceará];
+  //codigodacarta2(char)[B01];
+   //nomecidade2(char)[fortaleza];
+   //populacao2(int)[50000000];
+   // area2(float)[700000000];
+// pib1(float)[900000000];
+   // numeropontosturisticos2(foat)[54];
+   
+   // Exibição das informações da Carta 2
+   //printf("Carta 2:\n");
+   //printf("Estado: %c\n", estado2);
+   //printf("Código: %s\n", codigo2);
+   //printf("Nome da Cidade: %s\n", cidade2);
+   //printf("População: %d\n", populacao2);
+   //printf("Área: %.2f km²\n", area2);
+   //printf("PIB: %.2f bilhões de reais\n", pib2);
+   //printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+   
+   return 0;
+}
